4931fd44dfc5601937f1b675b86e1cae2a883d59
imabutahersiddik <abutahersiddik313@gmail.com>
2024-08-25 20:42:34 +0600

    Added middlewares

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,20 @@
+// File: ./src/middlewares/authMiddleware.js
+
+const jwt = require('jsonwebtoken');
+
+exports.authenticate = (req, res, next) => {
+  const token = req.headers['authorization']?.split(' ')[1]; // Bearer token
+
+  if (!token) {
+    return res.status(401).json({ error: 'No token provided' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Invalid token' });
+    }
+
+    req.user = { id: decoded.id }; // Store user ID in request for further use
+    next();
+  });
+};
